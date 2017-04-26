4a20b513629e9dcd57ffa6ab2b9c51ec59c08474
Hubert51 <ruijiegeng@gmail.com>
2017-04-25 20:31:28 -0400

    The whole class for pyLemma.
    In this class I implement many functions including add data, create
    tables, drop or delete tables, download and upload file.

--- Database_MySQL/mysql_initialize.py
+++ Database_MySQL/mysql_initialize.py
@@ -1,5 +1,6 @@
 import mysql.connector 
 from mysql.connector import errorcode
+import my_sql_class
 
 # This file is trying to test whether mysql
 try:
@@ -8,7 +9,7 @@ try:
 		user="root",
 		password="gengruijie",
 		host= "127.0.0.1",
-		database ="Student"
+		database ="Lemma"
 		)
 	print("It works!!")
 
@@ -21,3 +22,19 @@ except mysql.connector.Error as e:
 		print(e)
 
 
+hw = my_sql_class.Database_mysql("root","gengruijie","127.0.0.1","Lemma")
+# hw.create_hw_table()
+
+table_name = "student1_hw8"
+# information = [1,'ABCD',"123",21]
+# hw.add_hw_data(table_name,information)
+
+# hw.add_student_hw("Ruijie",1234,1234)
+# hw.delete("student1")
+# hw.drop(table_name)    # it does not exist any more.
+
+
+# hw.read_code("Ruijie","hw8")
+# hw.up_load_code("Ruijie","hw8","test.prf")
+
+hw.down_load_file("Ruijie","hw8","testout.prf")
